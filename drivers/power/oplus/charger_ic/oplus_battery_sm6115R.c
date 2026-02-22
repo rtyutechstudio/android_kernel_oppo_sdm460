23c8a9093488ec33624ae60a46f9d9d61babfcaa
wudilsr <wudilsr@gmail.com>
2026-02-22 17:08:15 +0800

    Fix -Wstrict-prototypes warnings in power and display drivers
    Add (void) to function declarations without parameters to comply
    with strict prototype checking.

--- drivers/power/oplus/charger_ic/oplus_battery_sm6115R.c
+++ drivers/power/oplus/charger_ic/oplus_battery_sm6115R.c
@@ -13583,7 +13583,7 @@ ntcvolt_get_done:
 	return batt_btb_tmp;
 }
 
-void oplus_set_typec_sinkonly()
+void oplus_set_typec_sinkonly(void)
 {
 	int rc;
 	struct smb_charger *chg = NULL;
--- drivers/power/oplus/charger_ic/oplus_sc8547.c
+++ drivers/power/oplus/charger_ic/oplus_sc8547.c
@@ -1532,7 +1532,7 @@ void oplus_vooc_reset_variables(void)
 	return;
 }
 
-BattMngr_err_code_e oplus_vooc_handle_ask_fastchg_ornot_cmd()
+BattMngr_err_code_e oplus_vooc_handle_ask_fastchg_ornot_cmd(void)
 {
 	BattMngr_err_code_e status = BATTMNGR_SUCCESS;
 
@@ -1591,7 +1591,7 @@ BattMngr_err_code_e oplus_vooc_handle_ask_fastchg_ornot_cmd()
 
 
 
-BattMngr_err_code_e oplus_vooc_calcucate_identification_code()
+BattMngr_err_code_e oplus_vooc_calcucate_identification_code(void)
 {
 	BattMngr_err_code_e status = BATTMNGR_SUCCESS;
 	UINT8 code_id_temp_l = 0, code_id_temp_h = 0;
@@ -1629,7 +1629,7 @@ BattMngr_err_code_e oplus_vooc_calcucate_identification_code()
 }
 
 
-BattMngr_err_code_e oplus_vooc_handle_identification_cmd()
+BattMngr_err_code_e oplus_vooc_handle_identification_cmd(void)
 {
 	BattMngr_err_code_e status = BATTMNGR_SUCCESS;
 
@@ -1653,7 +1653,7 @@ BattMngr_err_code_e oplus_vooc_handle_identification_cmd()
 
 
 
-BattMngr_err_code_e oplus_vooc_handle_adapter_check_commu_process_cmd()
+BattMngr_err_code_e oplus_vooc_handle_adapter_check_commu_process_cmd(void)
 {
 	BattMngr_err_code_e status = BATTMNGR_SUCCESS;
 
