dfc8f029defc2565e78581b4624ac86c64ef696b
wudilsr <wudilsr@gmail.com>
2026-03-15 16:34:41 +0800

    OPLUS: Add performance, power, security and peripheral drivers
    Add comprehensive OPLUS vendor-specific kernel modules from external archive:
    
    Performance Optimization (26 modules):
    - cpufreq_bouncing, foreground_io_opt, input_boost
    - iomonitor, lowmem_dbg, memory_isolate, multi_kswapd
    - sched_assist, special_opt, task_cpustats, tpd, tpp
    - uifirst, uxio_first, zwb_handle
    - healthinfo (with ion sub-monitoring)
    - im, memleak_detect, oplus_mm, oplus_nandswap
    - process_reclaim, gloom, gloom_new
    - ion_boost_pool, multi_freearea, vm_anti_fragment
    
    Power Management (4 modules):
    - cpufreq_effiency, cpufreq_health
    - midas (display/capability/power monitoring)
    - power_diag
    
    Security (3 modules):
    - keventupload, rootguard, securegki
    
    Peripheral Drivers:
    - aw8697_haptic: AWINIC haptic driver
    - oplus_gpio: GPIO control driver
    - colorctrl: Display color control (v1/v2)
    
    Build System:
    - Add Kconfig/Makefile for all modules
    - Update defconfig with OPLUS configs (157 total)
    - Update parent Kconfig/Makefile references

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -12,6 +12,7 @@
  * Copyright (c) 2009     Shrikar Archak
  * Copyright (c) 2003-2011 Stony Brook University
  * Copyright (c) 2003-2011 The Research Foundation of SUNY
+ * Copyright (C) 2020 Oplus. All rights reserved.
  *
  * This file is dual licensed.  It may be redistributed and/or modified
  * under the terms of the Apache 2.0 License OR version 2 of the GNU
@@ -165,7 +166,7 @@ int parse_options_remount(struct super_block *sb, char *options, int silent,
 	char *p;
 	substring_t args[MAX_OPT_ARGS];
 	int option;
-	int debug;
+	int debug = 0;
 
 	if (!options)
 		return 0;
